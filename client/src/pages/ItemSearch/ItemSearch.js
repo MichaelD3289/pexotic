863801614f22bd350f6fd1eb6a76d39d73791b68
Michael Drummond <michael.drummond328@gmail.com>
2022-04-13 20:28:41 -0600

    react-routes created

---
+++ client/src/pages/ItemSearch/ItemSearch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ItemSearch.css'
+
+function ItemSearch() {
+  return (
+    <div>ItemSearch</div>
+  )
+}
+
+export default ItemSearch
---
+++ client/src/redux/reducers/currentUser.js
@@ -0,0 +1,15 @@
+// constant type variables
+
+// action functions
+
+
+// reducer function
+export default function currentUserReducer(state={}, action) {
+  switch (action.type) {
+    // case statements
+
+    // default statement
+    default:
+      return state;
+  }
+}
